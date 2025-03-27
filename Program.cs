933f52b388561203b6513ad921a753112d03ffb5
Cal-ly <cal-ly@outlook.dk>
2025-03-27 12:16:20 +0100

    Refactor weather components and improve UI design
    - Simplified `RawChartJS.razor` by removing unnecessary `<div>`.
    - Updated `WeatherChart.razor` title and increased forecast days from 3 to 5; added new color variables for styling.
    - Enhanced `WeatherTable.razor` to display 5-day forecast with additional weather data.
    - Improved `NavMenu.razor` with a background color and border for better aesthetics.
    - Centered content in `About.razor` by adding a container.
    - Changed greeting in `Home.razor` for a friendlier tone.
    - Included `WeatherChart` component in `WeatherForecast.razor`.
    - Cleaned up unused using directives in `Program.cs`.
    - Modified button styles in `app.css` for better layout and alignment.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,7 @@
-using System.Reflection;
 using BlazorWasmGuitarScales;
-using BlazorWasmGuitarScales.Models;
 using BlazorWasmGuitarScales.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
